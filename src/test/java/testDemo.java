7fba3bdd845c1a97e33852f36a578e856b3fb433
13810765094@163.com <8023guawohuide>
2017-12-21 12:34:46 +0800

    首先创建项目

---
+++ src/test/java/testDemo.java
@@ -0,0 +1,18 @@
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * created by xiapf on 2017/12/21
+ */
+public class testDemo {
+    @Test
+    public void testcase1(){
+        Assert.assertTrue(false);
+        System.out.println("testcase1");
+    }
+    @Test
+    public void testcase2(){
+        Assert.assertTrue(true);
+        System.out.println("testcase1");
+    }
+}
